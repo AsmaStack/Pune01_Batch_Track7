9832c1f2c8f9aca599b66ffa3630851587985e7f
Prajwal Shrestha <prajwals@amdocs.com>
2023-08-31 16:46:27 +0530

    dependent object

--- src/main/java/springEx1/Customer.java
+++ src/main/java/springEx1/Customer.java
@@ -3,7 +3,14 @@ package springEx1;
 public class Customer {
 	private int cust_id;
 	private String cust_name;
+	private Address address; // dependent object
 	
+	public Customer(int cust_id, String cust_name, Address address) {
+		
+		this.cust_id = cust_id;
+		this.cust_name = cust_name;
+		this.address = address;
+	}
 	public Customer(int cust_id) {
 		this.cust_id = cust_id;
 	}
@@ -17,8 +24,9 @@ public class Customer {
 	}
 	@Override
 	public String toString() {
-		return "Customer [cust_id=" + cust_id + ", cust_name=" + cust_name + "]";
+		return "Customer [cust_id=" + cust_id + ", cust_name=" + cust_name + ", address=" + address + "]";
 	}
+	
 	
 	
 	
